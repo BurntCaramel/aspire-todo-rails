c585895e334a7e1e1d21572016ac292dab80b93b
Patrick Smith <patrick@burntcaramel.com>
2017-04-11 12:56:21 +1000

    Style with MuiCSS

--- app/helpers/todo_items_helper.rb
+++ app/helpers/todo_items_helper.rb
@@ -1,2 +1,9 @@
 module TodoItemsHelper
+  def completed_at_ago(item)
+    if item.completed_at.present?
+      "#{time_ago_in_words item.completed_at} ago"
+    else
+      "no"
+    end
+  end
 end
