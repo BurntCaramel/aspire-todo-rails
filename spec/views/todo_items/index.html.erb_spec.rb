2a96766c512077caef169fe2a68f121801eeef34
Patrick Smith <patrick@burntcaramel.com>
2017-04-11 17:05:55 +1000

    Fix todo_items #index spec

--- spec/views/todo_items/index.html.erb_spec.rb
+++ spec/views/todo_items/index.html.erb_spec.rb
@@ -2,18 +2,30 @@ require 'rails_helper'
 
 RSpec.describe "todo_items/index", type: :view do
   before(:each) do
-    assign(:todo_items, [
+    assign(:complete_items, [
       TodoItem.create!(
-        :title => "Title"
+        title: 'Completed',
+        completed_at: Time.current - 1.hours
       ),
       TodoItem.create!(
-        :title => "Title"
+        title: "Completed",
+        completed_at: Time.current - 2.minutes
+      )
+    ])
+
+    assign(:incomplete_items, [
+      TodoItem.create!(
+        title: 'Not Completed'
+      ),
+      TodoItem.create!(
+        title: "Not Completed"
       )
     ])
   end
 
   it "renders a list of todo_items" do
     render
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
+    assert_select "tr>td", text: "Completed".to_s, count: 2
+    assert_select "tr>td", text: "Not Completed".to_s, count: 2
   end
 end
