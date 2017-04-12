a5b32bd16ad8d016d20be26e3e34bea9b9d74498
Patrick Smith <patrick@burntcaramel.com>
2017-04-12 14:42:03 +1000

    Complete migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412032157) do
+ActiveRecord::Schema.define(version: 20170412034510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20170412032157) do
     t.datetime "completed_at"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "list_id"
+    t.index ["list_id"], name: "index_todo_items_on_list_id", using: :btree
   end
 
+  add_foreign_key "todo_items", "lists"
 end
